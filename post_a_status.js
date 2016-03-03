447263e5140958c2a385fae9cdd2404bfd32ae92
Gloria Bwandungi <30@holbertonschool.com>
2016-03-02 21:55:10 -0500

    Hidden status form displayed

---
+++ post_a_status.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    var showForm = document.getElementById('showLink');
+    showForm.onclick = function() {
+        document.getElementById('statusPost').style.display = 'block';
+    };
+};
